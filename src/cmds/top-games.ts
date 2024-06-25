9b076945e6459c06b95aca4a07266b8755e86a6e
Technoblade <william@williamgiles.co.nz>
2024-06-25 19:13:30 +1200

    Update top-games.ts

--- src/cmds/top-games.ts
+++ src/cmds/top-games.ts
@@ -4,6 +4,18 @@ import { getDuration, prettySeconds } from '../prettySeconds'
 import { inBlacklist } from '../groups'
 import prettyEmbeds from '../prettyEmbeds'
 
+// let UserTotals = {
+//   bob: {
+//     game: 300,
+//     game2: 100,
+//   },
+//   alice: {
+//     game: 200,
+//   },
+// }
+
+type UserTotals = Record<string, Record<string, number>>
+
 const prisma = new PrismaClient()
 
 async function handleTopGames(interaction: Discord.CommandInteraction) {
@@ -28,45 +40,72 @@ async function handleTopGames(interaction: Discord.CommandInteraction) {
     },
   })
 
-  let gameTotals: Record<string, number> = {}
-  let userTotals: Record<string, number> = {}
+  let gameTotals = new Map<string, number>()
+  let userTotals = new Map<string, Map<string, number>>()
   let userTotalsMap = new Map<string, string>()
 
   for (const user of users) {
     inner: for (const activity of user.activities) {
       const duration = getDuration(activity)
 
-      if (userTotals[activity.name]) {
-        userTotals[activity.name] += duration
-      } else {
-        userTotals[activity.name] = duration
+      if (!userTotals.has(user.username)) {
+        userTotals.set(user.username, new Map<string, number>())
       }
+
+      if (!userTotals.get(user.username).has(activity.name)) {
+        userTotals.get(user.username).set(activity.name, 0)
+      }
+
+      userTotals
+        .get(user.username)
+        .set(
+          activity.name,
+          userTotals.get(user.username).get(activity.name) + duration
+        )
 
       if (inBlacklist(activity.name)) continue inner
 
-      if (gameTotals[activity.name]) {
-        gameTotals[activity.name] += duration
-      } else {
-        gameTotals[activity.name] = duration
+      if (!gameTotals.has(activity.name)) {
+        gameTotals.set(activity.name, 0)
+      }
+
+      gameTotals.set(activity.name, gameTotals.get(activity.name) + duration)
+    }
+  }
+
+  let finalList: { game: string; duration: number, highestUser: string, userTime: number }[] = []
+
+  for (const [game, duration] of gameTotals) {
+    let highestUser = ''
+    let userTime = 0
+
+    for (const [user, games] of userTotals) {
+      for (const [gameName, time] of games) {
+        if (gameName == game && time > userTime) {
+          highestUser = user
+          userTime = time
+        }
       }
     }
+
+    finalList.push({ game, duration, highestUser, userTime })
   }
 
-  let gameTotalsArray = Object.entries(gameTotals)
-  gameTotalsArray.sort((a, b) => b[1] - a[1])
+  // let gameTotalsArray = Object.entries(gameTotals)
+  // gameTotalsArray.sort((a, b) => b[1] - a[1])
 
-  let userTotalsArray = Object.entries(userTotals)
+  // let userTotalsArray = Object.entries(userTotals)
 
-  for (const [game, duration] of userTotalsArray) {
-    let user = users.find((user) => {
-      let activity = user.activities.find((activity) => activity.name == game)
-      return activity
-    })
+  // for (const [game, duration] of userTotalsArray) {
+  //   let user = users.find((user) => {
+  //     let activity = user.activities.find((activity) => activity.name == game)
+  //     return activity
+  //   })
 
-    if (user) {
-      userTotalsMap.set(game, user.username)
-    }
-  }
+  //   if (user) {
+  //     userTotalsMap.set(game, user.username)
+  //   }
+  // }
 
   // create embed
 
@@ -75,16 +114,15 @@ async function handleTopGames(interaction: Discord.CommandInteraction) {
     .setColor('Random')
 
   let i = 1
-  for (const [game, duration] of gameTotalsArray) {
+  for (const item of finalList) {
     if (i > 10) {
       break
     }
 
-    let user = userTotalsMap.get(game)
 
     embed.addFields({
-      name: `${i}. ${game}`,
-      value: `${prettySeconds(duration)}\n${user}`,
+      name: `${i}. ${item.game}`,
+      value: `${prettySeconds(item.duration)}\n${item.highestUser}(${prettySeconds(item.userTime)})`,
       inline: false,
     })
 
